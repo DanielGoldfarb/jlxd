344039f04b9fab3cdb7832d4f3b2e30881ad40d9
Daniel Goldfarb <dgoldfarb.github@gmail.com>
2023-04-28 19:01:09 -0400

    STEP1: create a command to do_something

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,20 @@ const plugin: JupyterFrontEndPlugin<void> = {
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
     console.log('JupyterLab extension jlxd is activated!');
+
+    let commandId = 'jlxd:Hello';
+    app.commands.addCommand(commandId,
+       { label: 'Hello World',
+         execute: do_something
+       });
+
+    app.commands.execute(commandId);
+
   }
 };
+
+function do_something() {
+   console.log('jlxd says "Hello World!"');
+}
 
 export default plugin;
