9e56a4094cc3113f3e66d32f72431a73aff48229
Daniel Goldfarb <dgoldfarb.github@gmail.com>
2023-04-28 19:08:30 -0400

    STEP3a: split out activate function

--- src/index.ts
+++ src/index.ts
@@ -12,22 +12,24 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jlxd:plugin',
   autoStart: true,
   requires: [ ICommandPalette ],
-  activate: (app: JupyterFrontEnd,
-             palette: ICommandPalette) => {
-    console.log('JupyterLab extension jlxd is activated!');
-
-    let commandId = 'jlxd:Hello';
-    app.commands.addCommand(commandId,
-       { label: 'Hello World',
-         execute: do_something
-       });
-
-    palette.addItem( {
-       command: commandId,
-       category: 'Anything'
-       });
-  }
+  activate: _activate
 };
+
+function _activate(app: JupyterFrontEnd,
+                   palette: ICommandPalette) {
+   console.log('JupyterLab extension jlxd is activating!');
+
+   let commandId = 'jlxd:Hello';
+   app.commands.addCommand(commandId,
+      { label: 'Hello World',
+        execute: do_something
+      });
+
+   palette.addItem( {
+      command: commandId,
+      category: 'Anything'
+      });
+}
 
 function do_something() {
    console.log('jlxd says "Hello World!"');
